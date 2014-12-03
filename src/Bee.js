b290512e6e28ac33f592abfb78020bc4d6d773b6
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-12-03 10:48:46 -0800

    Instantiated all bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+	// var bee = Grub();
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = "Keep on growing";
+	// return bee; 
+
+
 };
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Grub;
+
 
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,18 @@
 var Grub = function() {
+	// var obj = {};
+	// obj.age = 0; 
+	// obj.pink = 'pink';
+	// obj.food = 'jelly';
+	// obj.eat = function() {
+
+	// }
+	// return obj; 
+
+	this.age = 0;
+	this.pink = 'pink';
+	this.food = 'jelly';
+	this.eat = function() {
+
+	}
 };
 
